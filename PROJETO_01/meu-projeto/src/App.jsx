1bb947042e8517c4e5efaa225c72ef4a323f8b1a
PedroMachadoGRP <pedrolimass10@gmail.com>
2025-10-20 17:10:13 -0300

    add

---
+++ PROJETO_01/meu-projeto/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className='w-[100vw] h-[50vh] bg-amber-500'>
+
+    </div>
+    </>
+  )
+}
+
+export default App
